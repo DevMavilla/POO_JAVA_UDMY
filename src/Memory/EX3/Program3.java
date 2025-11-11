721032ec67f5f98e99d25996a01523eccadc3271
Diego Avila <diegomoraesavilla99@gmail.com>
2025-11-10 22:24:56 -0300

    Criação do métodos no obj Pessoa

---
+++ src/Memory/EX3/Program3.java
@@ -0,0 +1,48 @@
+package Memory.EX3;
+
+import Memory.EX3.entities.People;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program3 {
+    public static void main (String[] args){
+            Locale.setDefault(Locale.US);
+            Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantas pessoas serão digitadas ");
+            int n = sc.nextInt();
+            People[] people = new People[n];
+
+            for (int i = 0; i < people.length; i++) {
+                System.out.printf("Dados da %da pessoa:\n", i + 1);
+                sc.nextLine();
+
+                System.out.print("Name: ");
+                String name = sc.nextLine();
+
+                System.out.print("Age: ");
+                int age = sc.nextInt();
+
+                System.out.print("Height: ");
+                double height = sc.nextDouble();
+
+                people[i] = new People(name, age, height);
+            }
+
+        double mediaAltura = People.calcularMediaAltura(people);
+        int menores = People.contarMenoresDe16(people);
+        double percentual = People.percentualMenoresDe16(people);
+
+        System.out.printf("Altura média - %.2f%n", mediaAltura);
+        System.out.printf("Pessoas com menos de 16 anos: %d (%.1f%%)%n", menores, percentual);
+
+        for (int i = 0; i < people.length; i++) {
+            if (people[i].getAge() < 16) {
+                System.out.println(people[i].getName());
+            }
+        }
+
+        sc.close();
+    }
+}
