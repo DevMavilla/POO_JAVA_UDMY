edcc7938488a38ed5b52ed0c7ef498562ec1aced
Diego Avila <diegomoraesavilla99@gmail.com>
2026-01-13 22:06:04 -0300

    Exercício sobre StringBuilder

--- src/Composicao/entities/Worker.java
+++ src/Composicao/entities/Worker.java
@@ -10,7 +10,6 @@ public class Worker {
     private String name;
     private WorkerLevel level;
     private Double baseSalary;
-
     private Department department;
     private List<HourContract> contracts = new ArrayList<>();
 
