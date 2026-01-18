cc023fb9713683330829c8a7b2efe95e6609ec7f
Diego Avila <diegomoraesavilla99@gmail.com>
2026-01-18 12:03:32 -0300

    Sobreposição de método withDraw

--- src/Java/Heranca/apresentacao/entities/SavingsAccount.java
+++ src/Java/Heranca/apresentacao/entities/SavingsAccount.java
@@ -23,4 +23,9 @@ public class SavingsAccount extends Account {
     public void updateBalance(){
         balance += balance * interestRate;
     }
+
+    @Override
+    public void whithdraw(Double amount){
+        balance -= amount;
+    }
 }
